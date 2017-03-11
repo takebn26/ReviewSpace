337fe61daf88249852032d21960b08d04a65d578
takebn26 <takehaya1026@gmail.com>
2017-03-12 08:24:12 +0900

    edit group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,7 @@
 class GroupsController < ApplicationController
+
+  before_action :set_group, only: [:edit, :update]
+
   def new
     @group = Group.new
   end
@@ -14,9 +17,25 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループ更新成功'
+    else
+      flash.now[:alert] = 'グループ更新失敗'
+      render :edit
+    end
+  end
+
   private
 
   def group_params
     params.require(:group).permit(:name)
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:new, :create, :edit, :update]
 end
