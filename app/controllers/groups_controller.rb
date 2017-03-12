317394319358b9a457abca42198481733c7ad107
takebn26 <takehaya1026@gmail.com>
2017-03-12 11:18:27 +0900

    fix groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -27,7 +27,7 @@ class GroupsController < ApplicationController
 
   def update
     if @group.update(group_params)
-      redirect_to group_messages_path(@group), notice: 'グループ更新成功'
+      redirect_to root_path, notice: 'グループ更新成功'
     else
       flash.now[:alert] = 'グループ更新失敗'
       render :edit
