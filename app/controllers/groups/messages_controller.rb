14b98396ca2976d57fc8067c2ec34d333a446c1a
takebn26 <takehaya1026@gmail.com>
2017-03-16 13:18:14 +0900

    show messages on index view

--- app/controllers/groups/messages_controller.rb
+++ app/controllers/groups/messages_controller.rb
@@ -1,6 +1,7 @@
 class Groups::MessagesController < ApplicationController
   def index
-    @group = current_user.groups.find(params[:group_id])
+    @group  = current_user.groups.find(params[:group_id])
     @groups = current_user.groups
+    @messages = @group.messages.includes(:user)
   end
 end
